fa5446af17f80af5facb5f47ac7faf943d1b6207
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-08-27 08:57:11 +0200

    Added eager fetch function to find applications with all their documents

--- backend/ResursPortal/src/main/java/se/comerit/resurs/repository/ApplicationRepository.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/repository/ApplicationRepository.java
@@ -2,7 +2,9 @@ package se.comerit.resurs.repository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import se.comerit.resurs.entity.Application;
@@ -13,4 +15,7 @@ public interface ApplicationRepository extends JpaRepository<Application, Long> 
     List<Application> findByStatus(ApplicationStatus status);
     List<Application> findByStatusOrderByCreatedAtAsc(ApplicationStatus status);
     List<Application> findTop20ByStatusInOrderByUpdatedAtDesc(Collection<ApplicationStatus> statuses);
+
+    @EntityGraph(attributePaths = {"company", "documents"})
+    Optional<Application> findByIdWithDocuments(Long id);
 }
