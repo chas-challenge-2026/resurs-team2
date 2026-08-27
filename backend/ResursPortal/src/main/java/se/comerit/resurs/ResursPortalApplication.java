008fce8187484fc173d95d85b18a23e425ef1aa5
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-08-27 13:30:05 +0200

    Enabled scheduling which is needed for the session

--- backend/ResursPortal/src/main/java/se/comerit/resurs/ResursPortalApplication.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/ResursPortalApplication.java
@@ -2,7 +2,9 @@ package se.comerit.resurs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ResursPortalApplication {
 
