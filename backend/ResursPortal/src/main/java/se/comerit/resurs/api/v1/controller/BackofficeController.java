96e0de90890603fc2070b82c1748fc406e45d45f
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-08-31 12:21:47 +0200

    Added reading caseworker from principle

--- backend/ResursPortal/src/main/java/se/comerit/resurs/api/v1/controller/BackofficeController.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/api/v1/controller/BackofficeController.java
@@ -1,6 +1,7 @@
 package se.comerit.resurs.api.v1.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import se.comerit.resurs.api.v1.dto.ApplicationDetailsResponse;
 import se.comerit.resurs.api.v1.service.BackofficeService;
+import se.comerit.resurs.security.UserPrincipal;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,9 +39,9 @@ public class BackofficeController {
     }
     
     @GetMapping("application/{id}")
-    public ResponseEntity<ApplicationDetailsResponse> viewApplicationDetails(@PathVariable Long id) {
-        // TODO
-        String caseWorker = null;
+    public ResponseEntity<ApplicationDetailsResponse> viewApplicationDetails(@PathVariable Long id,
+            @AuthenticationPrincipal UserPrincipal principal) {
+        String caseWorker = principal.asCaseWorker().name();
         return service.viewApplicationDetails(id, caseWorker);
     }
     
