e27629c01ceeef2897b2e8e6b77f2988f0f0de31
IngvaldssonMattias <mattias.ingvaldsson@chasacademyy.se>
2026-09-01 11:53:49 +0200

    refactor document.html into React Typescript. fixed styling in credit-application. fixed styling in header. created Layout file for header. put 3 colors into file tokens.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login/Login";
+import { Layout } from "./pages/Layout/Layout";
+import { Documents } from "./components/documents/Documents";
+import { CreditApplication } from "./components/credit-application/CreditApplication";
 
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route element={<Layout />} >
+
+
+        <Route path="/documents" element={<Documents />} />
+        <Route path="/credit-application" element={<CreditApplication />} />
+        
+        
+        </Route>
       </Routes>
     </BrowserRouter>
   );
