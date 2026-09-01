05830b50b39f070d30537ae0ea6619f3691599a2
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-09-01 08:55:40 +0200

    Added more explicit safety annotations

--- backend/ResursPortal/src/main/java/se/comerit/resurs/api/v1/controller/BackofficeController.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/api/v1/controller/BackofficeController.java
@@ -1,13 +1,17 @@
 package se.comerit.resurs.api.v1.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
 import se.comerit.resurs.api.v1.dto.ApplicationDetailsResponse;
 import se.comerit.resurs.api.v1.dto.ApplicationOverview;
 import se.comerit.resurs.api.v1.dto.ApplicationResponse;
@@ -15,11 +19,9 @@ import se.comerit.resurs.api.v1.dto.DecisionRequest;
 import se.comerit.resurs.api.v1.service.BackofficeService;
 import se.comerit.resurs.security.UserPrincipal;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 @RestController
 @RequestMapping("api/v1/backoffice")
+@Validated
 public class BackofficeController {
 
     private final BackofficeService service;
@@ -28,6 +30,7 @@ public class BackofficeController {
         this.service = service;
     }
 
+    @PreAuthorize("hasRole('CASE_WORKER')")
     @GetMapping
     public ResponseEntity<ApplicationOverview> overview(
         @AuthenticationPrincipal UserPrincipal principal
@@ -36,6 +39,7 @@ public class BackofficeController {
         return ResponseEntity.ok(service.applicationOverview(caseWorker));
     }
 
+    @PreAuthorize("hasRole('CASE_WORKER')")
     @PostMapping("decide")
     public ResponseEntity<ApplicationResponse> decide(@RequestBody @Valid DecisionRequest request,
             @AuthenticationPrincipal UserPrincipal principal) {
@@ -43,6 +47,7 @@ public class BackofficeController {
         return ResponseEntity.ok(service.decide(request, caseWorker));
     }
 
+    @PreAuthorize("hasRole('CASE_WORKER')")
     @GetMapping("application/{id}")
     public ResponseEntity<ApplicationDetailsResponse> viewApplicationDetails(@PathVariable Long id,
             @AuthenticationPrincipal UserPrincipal principal) {
