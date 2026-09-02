3498d3950577c180e3c2d8de176b1ec5afe20c24
Oliver Öhrström <xzereha@users.noreply.github.com>
2026-09-02 10:56:26 +0200

    Wired up controller

--- backend/ResursPortal/src/main/java/se/comerit/resurs/api/v1/controller/ApplicationController.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/api/v1/controller/ApplicationController.java
@@ -1,22 +1,29 @@
 package se.comerit.resurs.api.v1.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import se.comerit.resurs.api.v1.dto.CurrentCompanyResponse;
-import se.comerit.resurs.security.UserPrincipal;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import se.comerit.resurs.api.v1.dto.ApplicationRequest;
+import se.comerit.resurs.api.v1.dto.CurrentCompanyResponse;
+import se.comerit.resurs.api.v1.service.ApplicationService;
+import se.comerit.resurs.security.UserPrincipal;
 
 @RestController
 @RequestMapping("api/v1/application")
 public class ApplicationController {
+
+    private final ApplicationService service;
+
+    public ApplicationController(ApplicationService service) {
+        this.service = service;
+    }
 
     @PreAuthorize("hasRole('COMPANY')")
     @GetMapping("apply")
@@ -29,10 +36,15 @@ public class ApplicationController {
 
     @PreAuthorize("hasRole('COMPANY')")
     @PostMapping("apply")
-    public String submit(@RequestBody String entity) {
-        // TODO: process POST request
-
-        return entity;
+    public ResponseEntity<Long> submit(@RequestBody ApplicationRequest application,
+            @AuthenticationPrincipal UserPrincipal principal) {
+        // NOTE: Currently requires company to be registered, the original would create
+        // the company if missing
+        // TODO: Consider what to do, maybe check if data matches database otherwise
+        // register new company.
+        var company = principal.asCompany();
+        String orgNumber = company.orgNumber();
+        return ResponseEntity.ok(service.submitApplication(orgNumber, application));
     }
 
     @PreAuthorize("hasRole('COMPANY')")
