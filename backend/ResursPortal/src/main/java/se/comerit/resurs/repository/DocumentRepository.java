face3d72fd044c4e9db8938da975b8da9228c58a
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2026-09-10 14:59:28 +0200

    fix: change document ID type from Long to UUID in DocumentRepository

--- backend/ResursPortal/src/main/java/se/comerit/resurs/repository/DocumentRepository.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/repository/DocumentRepository.java
@@ -1,12 +1,17 @@
 package se.comerit.resurs.repository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import se.comerit.resurs.entity.Document;
 
-public interface DocumentRepository extends JpaRepository<Document, Long> {
+public interface DocumentRepository extends JpaRepository<Document, UUID> {
     List<Document> findByApplicationId(Long applicationId);
     List<Document> findByApplicationIdOrderByUploadedAtDesc(Long applicationId);
+
+
+    Optional<Document>findByUuid(UUID uuid);
 }
