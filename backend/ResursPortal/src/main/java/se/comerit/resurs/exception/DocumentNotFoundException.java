eec331d84e183e594738204b0da5bfda41de4d1c
nicolaslovgren92 <nicolas.lovgren92@gmail.com>
2026-09-10 14:59:43 +0200

    fix: change document ID type from Long to UUID in DocumentNotFoundException

--- backend/ResursPortal/src/main/java/se/comerit/resurs/exception/DocumentNotFoundException.java
+++ backend/ResursPortal/src/main/java/se/comerit/resurs/exception/DocumentNotFoundException.java
@@ -1,7 +1,9 @@
 package se.comerit.resurs.exception;
 
+import java.util.UUID;
+
 public class DocumentNotFoundException extends RuntimeException {
-    public DocumentNotFoundException(Long id) {
+    public DocumentNotFoundException(UUID id) {
         super("Document not found: id=" + id);
     }
 }
